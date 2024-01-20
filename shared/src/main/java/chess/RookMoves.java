d3dd9ae9e71cde1dd3a3eef937166c20fde6f333
Tommy <tommykloosterman@gmail.com>
2024-01-20 15:19:40 -0700

    completed rook moves tests

---
+++ shared/src/main/java/chess/RookMoves.java
@@ -0,0 +1,38 @@
+package chess;
+
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RookMoves {
+  public static Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition, ChessPiece.PieceType pieceType, ChessGame.TeamColor teamColor) {
+    List<ChessMove> moves = new ArrayList<>();
+    // Directions for horizontal and vertical moves
+    int[][] directions = {
+            {1, 0}, {0, 1}, {-1, 0}, {0, -1}
+    };
+
+    for (int[] direction : directions) {
+      int row = myPosition.getRow();
+      int column = myPosition.getColumn();
+      while (true) {
+        row += direction[0];
+        column += direction[1];
+        ChessPosition newPosition = new ChessPosition(row, column);
+        if (!board.isPositionValid(newPosition)) {
+          break;
+        }
+        if (board.isPieceAt(newPosition)) {
+          if (board.getPiece(newPosition).getTeamColor() != teamColor) {
+            moves.add(new ChessMove(myPosition, newPosition, null)); // Capturing move
+          }
+          break; // Blocked by a piece
+        } else {
+          moves.add(new ChessMove(myPosition, newPosition, null)); // Free move
+        }
+      }
+    }
+
+    return moves;
+  }
+}
