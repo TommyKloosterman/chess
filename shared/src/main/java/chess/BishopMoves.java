116df40255bf1d80386309cc1d40f6fb60230a9a
Tommy <tommykloosterman@gmail.com>
2024-09-19 10:04:35 -0600

    completed bishop moves

---
+++ shared/src/main/java/chess/BishopMoves.java
@@ -0,0 +1,32 @@
+package chess;
+
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BishopMoves {
+  public static Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition, ChessPiece.PieceType pieceType, ChessGame.TeamColor teamColor) {
+    List<ChessMove> moves = new ArrayList<>();
+    int[][] directions = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}}; // The four diagonal directions
+
+    for (int[] direction : directions) {
+      int deltaX = direction[0];
+      int deltaY = direction[1];
+      ChessPosition position = new ChessPosition(myPosition.getRow() + deltaX, myPosition.getColumn() + deltaY);
+
+      while (board.isPositionValid(position)) {
+        if (!board.isPieceAt(position)) {
+          moves.add(new ChessMove(myPosition, position, null));
+        } else {
+          if (board.getPiece(position).getTeamColor() != teamColor) {
+            moves.add(new ChessMove(myPosition, position, null));
+          }
+          break;
+        }
+        position = new ChessPosition(position.getRow() + deltaX, position.getColumn() + deltaY);
+      }
+    }
+    return moves;
+  }
+}
+
